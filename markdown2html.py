40304d8e528d8e2e1a7df73cca772005931876cf
niranois13 <8784@holbertonstudents.com>
2024-12-09 15:03:55 +0100

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -2,23 +2,22 @@
 import sys
 import os
 
-def main():
+def main(markdown_file, output_file):
     """
     Function to convert markdown into HTML
-    :params argv[1]: markdown entrypoint file name
-    :params argv[2]: output file name
+    :params argv[1]: str - markdown entrypoint file name
+    :params argv[2]: str - output file name
     Returns: 0 on success, 1 if markdown file doesn't exists or on missing argument"""
+
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
-    markdown_file = sys.argv[1]
-
     if not os.path.exists(markdown_file):
-        print(f"Missing README.md", file=sys.stderr)
+        print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
     sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
